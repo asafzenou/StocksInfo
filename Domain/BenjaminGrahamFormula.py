49b7e3b985368339e8f084d6f69394cdc9c57da5
asafzenou <asafzenou97@gmail.com>
2024-10-24 17:53:19 +0300

    adding benjamin graham

--- Domain/BenjaminGrahamFormula.py
+++ Domain/BenjaminGrahamFormula.py
@@ -1,2 +1,17 @@
 
 class BenjaminGrahamFormula:
+    def __init__(self, user_input_yield, user_input_future_growth, eps):
+        self.__user_input_yield = user_input_yield
+        self.__user_input_future_growth = user_input_future_growth
+        self.__eps = eps
+
+    def get_range(self):
+        return self.__calc_lower_range(), self.__calc_upper_range()
+
+    def __calc_upper_range(self):
+        upper = self.__eps * (8.5 + (2 * self.__user_input_future_growth) * (4.4/self.__user_input_yield))
+        return upper
+
+    def __calc_lower_range(self):
+        lower = self.__eps * (7.5 + (1.5 * self.__user_input_future_growth) * (4.4 / self.__user_input_yield))
+        return lower
